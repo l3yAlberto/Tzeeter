417a637de29bae1018fe5d38d74a843fe3f86a29
l3yAlberto <junioralberto2127@gmail.com>
2022-05-31 10:15:17 -0300

    Lista 04

---
+++ index.js
@@ -0,0 +1,37 @@
+const home = document.getElementById("home");
+const bntTzeet = document.getElementById("bntTzeet");
+inputEvent(home);
+inputEvent(bntTzeet);
+
+/**
+ * @param { HTMLElement } div 
+ */
+function inputEvent(div) {
+    const textArea = div.querySelector("[contentEditable=true]");
+    const buttonEnviar = div.querySelector("button");
+    const countSpan = div.querySelector("span");
+
+    // console.log(div, textArea, buttonEnviar, countSpan);
+
+    textArea.addEventListener('blur', () => { /^\s$/.test(textArea.innerText) && (textArea.innerText = "") });
+
+    textArea.addEventListener("input", (ev) => {
+        // console.log(ev);
+        const textIsEmpty = !/\S/.test(textArea.innerText);
+        buttonEnviar.disabled = textIsEmpty;
+        const restanteCaracter = (140 - textArea.innerText.length);
+
+        if (!textIsEmpty && restanteCaracter < 40) {
+            countSpan.textContent = restanteCaracter;
+            if (restanteCaracter < 0) {
+                countSpan.style.color = "rgb(255, 0, 0)";
+            } else {
+                countSpan.style.color = "rgb(255, 200, 0)";
+            }
+        } else {
+            countSpan.textContent = "";
+        }
+        // console.log(textIsEmpty, restanteCaracter);
+    
+    })   
+}
